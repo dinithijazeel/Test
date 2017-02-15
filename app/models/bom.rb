7149caba9af071491688931605fbe1b01af6239c
Dinithi Jazeel <dinithij@atlinkcom.com>
2017-02-15 13:58:16 +0530

    Signed-off-by: Dinithi Jazeel <dinithij@atlinkcom.com>

--- app/models/bom.rb
+++ app/models/bom.rb
@@ -170,35 +170,35 @@ class Bom < ActiveRecord::Base
 		#:ItemList => line_item_array
 	} 
   
-    # # Calculate taxes
-    # federal_tax_amount = invoice_total * 0.12
-    # state_tax_amount = invoice_total * 0.05
-    # local_tax_amount = invoice_total * 0.06
-    # # Get products from sku's
-    # federal_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:federal_tax])
-    # state_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:state_tax])
-    # local_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:local_tax])
-    # # Create array of line items to return
-    # [
-      # LineItem.new(
-        # description: federal_tax_product.description,
-        # quantity: 1,
-        # unit_price: federal_tax_amount,
-        # product: federal_tax_product
-      # ),
-      # LineItem.new(
-        # description: state_tax_product.description,
-        # quantity: 1,
-        # unit_price: state_tax_amount,
-        # product: state_tax_product
-      # ),
-      # LineItem.new(
-        # description: local_tax_product.description,
-        # quantity: 1,
-        # unit_price: local_tax_amount,
-        # product: local_tax_product
-      # ),
-    # ]
+    # Calculate taxes
+    federal_tax_amount = invoice_total * 0.12
+    state_tax_amount = invoice_total * 0.05
+    local_tax_amount = invoice_total * 0.06
+    # Get products from sku's
+    federal_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:federal_tax])
+    state_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:state_tax])
+    local_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:local_tax])
+    # Create array of line items to return
+    [
+      LineItem.new(
+        description: federal_tax_product.description,
+        quantity: 1,
+        unit_price: federal_tax_amount,
+        product: federal_tax_product
+      ),
+      LineItem.new(
+        description: state_tax_product.description,
+        quantity: 1,
+        unit_price: state_tax_amount,
+        product: state_tax_product
+      ),
+      LineItem.new(
+        description: local_tax_product.description,
+        quantity: 1,
+        unit_price: local_tax_amount,
+        product: local_tax_product
+      ),
+    ]
   end
 
   # View Helpers
