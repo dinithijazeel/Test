b1f16feeb03b7407faa3fa739cdacebf9d8f4a08
Dinithi Jazeel <dinithij@atlinkcom.com>
2017-02-15 13:54:31 +0530

    Signed-off-by: Dinithi Jazeel <dinithij@atlinkcom.com>

--- app/models/bom.rb
+++ app/models/bom.rb
@@ -153,35 +153,52 @@ class Bom < ActiveRecord::Base
   end
 
   def get_rating_line_items
-    # Calculate taxes
-    federal_tax_amount = invoice_total * 0.12
-    state_tax_amount = invoice_total * 0.05
-    local_tax_amount = invoice_total * 0.06
-    # Get products from sku's
-    federal_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:federal_tax])
-    state_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:state_tax])
-    local_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:local_tax])
-    # Create array of line items to return
-    [
-      LineItem.new(
-        description: federal_tax_product.description,
-        quantity: 1,
-        unit_price: federal_tax_amount,
-        product: federal_tax_product
-      ),
-      LineItem.new(
-        description: state_tax_product.description,
-        quantity: 1,
-        unit_price: state_tax_amount,
-        product: state_tax_product
-      ),
-      LineItem.new(
-        description: local_tax_product.description,
-        quantity: 1,
-        unit_price: local_tax_amount,
-        product: local_tax_product
-      ),
-    ]
+    #generate main hash for SureTax API call 
+	main_hash = {:ClientNumber => '000000870',
+		:BusinessUnit => '',
+		:ValidationKey => '13290031-F004-4F00-BMN3-E979D6749B88',
+		:DataYear => self.invoice_date.strftime("%Y"),
+		:DataMonth => self.invoice_date.strftime("%m"),
+		:CmplDataYear => self.invoice_date.strftime("%Y"), #?
+		:CmplDataMonth => self.invoice_date.strftime("%m"), #?
+		:TotalRevenue => invoice_total,
+		:ClientTracking =>  invoice.contact.portal_id,
+		:ResponseType => 'D2', #?
+		:ResponseGroup => '00',
+		:ReturnFileCode => '0',  
+		:STAN => ''#, #?
+		#:ItemList => line_item_array
+	} 
+  
+    # # Calculate taxes
+    # federal_tax_amount = invoice_total * 0.12
+    # state_tax_amount = invoice_total * 0.05
+    # local_tax_amount = invoice_total * 0.06
+    # # Get products from sku's
+    # federal_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:federal_tax])
+    # state_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:state_tax])
+    # local_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:local_tax])
+    # # Create array of line items to return
+    # [
+      # LineItem.new(
+        # description: federal_tax_product.description,
+        # quantity: 1,
+        # unit_price: federal_tax_amount,
+        # product: federal_tax_product
+      # ),
+      # LineItem.new(
+        # description: state_tax_product.description,
+        # quantity: 1,
+        # unit_price: state_tax_amount,
+        # product: state_tax_product
+      # ),
+      # LineItem.new(
+        # description: local_tax_product.description,
+        # quantity: 1,
+        # unit_price: local_tax_amount,
+        # product: local_tax_product
+      # ),
+    # ]
   end
 
   # View Helpers
