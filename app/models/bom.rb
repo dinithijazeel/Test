e921804372490c13f14bceb02e6f8b157c0bb2a2
Dinithi Jazeel <dinithij@atlinkcom.com>
2017-02-16 14:14:31 +0530

    Signed-off-by: Dinithi Jazeel <dinithij@atlinkcom.com>

--- app/models/bom.rb
+++ app/models/bom.rb
@@ -161,7 +161,7 @@ class Bom < ActiveRecord::Base
 		line_item_hash ={:Item => [:LineNumber => i,
 									:InvoiceNumber => number,
 									:CustomerNumber => contact.portal_id,
-									:TransDate => '' ,# invoice_date.strftime("%m/%d/%Y") ,
+									:TransDate => '2017-02-15T12:00:27' ,# invoice_date.strftime("%m/%d/%Y") ,
 									:BillingPeriodStartDate => '', #?
 									:BillingPeriodEndDate => '',#?
 									:Revenue => line_item.total.to_s,
