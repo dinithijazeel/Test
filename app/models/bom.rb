b4dbdbf835b33ced4c92056fa6ed510696da5f48
Dinithi Jazeel <dinithij@atlinkcom.com>
2017-02-28 15:40:53 +0530

    Signed-off-by: Dinithi Jazeel <dinithij@atlinkcom.com>

--- app/models/bom.rb
+++ app/models/bom.rb
@@ -263,7 +263,7 @@ class Bom < ActiveRecord::Base
 			parsed["GroupList"].each do |group| 
 				group["TaxList"].each do |tax|
 				puts "ooooooooooooooooooooooooooooooooooo"
-				 federal_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:tax["TaxTypeCode"]])
+				 federal_tax_product = Product.find_by_sku(Rails.application.config.x.products.special_products[:" #{tax["TaxTypeCode"]}"])
 				 puts federal_tax_product.name
 				 
 					puts tax["TaxTypeCode"]
