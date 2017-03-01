597698fcdb30e85a5068dfa8372f9e4e8dc72383
Dinithi Jazeel <dinithij@atlinkcom.com>
2017-03-01 11:52:30 +0530

    Signed-off-by: Dinithi Jazeel <dinithij@atlinkcom.com>

--- app/models/bom.rb
+++ app/models/bom.rb
@@ -265,7 +265,7 @@ class Bom < ActiveRecord::Base
 				puts "ooooooooooooooooooooooooooooooooooo"
 					tax_product = Product.find_by_sku(Rails.application.config.x.products.tax_products[':6666666666'])  
 					#tax_product = Product.find_by_sku(Rails.application.config.x.products.tax_products[tax["TaxTypeCode"].to_s.to_sym]))  
-					if tax_product.empty
+					if tax_product.empty?
 						puts "Invalid Tax Code : #{tax["TaxTypeCode"]}"
 					else
 						p = LineItem.new(
