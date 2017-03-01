a7a0c5843e34c7d8f1eff5878decbd5a03f93497
Dinithi Jazeel <dinithij@atlinkcom.com>
2017-03-01 16:58:56 +0530

    Signed-off-by: Dinithi Jazeel <dinithij@atlinkcom.com>

--- app/models/bom.rb
+++ app/models/bom.rb
@@ -271,17 +271,17 @@ class Bom < ActiveRecord::Base
 						if tax["TaxRate"] !=0 
 							#check whether the tax product is already added to the array
 							existing_tax_product = new_line_item_array.find {|s| s.description == tax["TaxTypeDesc"]} #DJ TODO Need to check by TaxCode
-							if existing_tax_product.nil?
+							#if existing_tax_product.nil?
 								p = LineItem.new(
 									description: tax["TaxTypeDesc"],
 									quantity: 1,
 									unit_price: tax["TaxRate"],  
 									product:  current_tax_product)
 								new_line_item_array.push(p)
-							else 
-								existing_tax_product.quantity = existing_tax_product.quantity + 1.to_f
-								existing_tax_product.unit_price = existing_tax_product.unit_price + tax["TaxRate"] 
-							end
+							#else 
+							#	existing_tax_product.quantity = existing_tax_product.quantity + 1.to_f
+							#	existing_tax_product.unit_price = existing_tax_product.unit_price + tax["TaxRate"] 
+							#end
 						end 
 					end
 				end
