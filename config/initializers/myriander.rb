1d78140de33c034910d0b2b20d8a74b1fd69329f
Dinithi Jazeel <dinithij@atlinkcom.com>
2017-03-06 16:21:28 +0530

    Signed-off-by: Dinithi Jazeel <dinithij@atlinkcom.com>

--- config/initializers/myriander.rb
+++ config/initializers/myriander.rb
@@ -1,9 +1,5 @@
 #
 # Load profile
 #
-
-puts "5555555555555555555555555555555555555555555555555555"
-puts Rails.root.join('config/profiles', "#{ENV['TENANT']}/#{ENV['TENANT']}.yml").to_s
-puts "5555555555555555555555555555555555555555555555555555"
 Conf.add_source!(Rails.root.join('config/profiles', "#{ENV['TENANT']}/#{ENV['TENANT']}.yml").to_s)
 Conf.reload!
